8b724c7e2e9b6c066c97c87d8aad175b697353b4
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-01-22 21:25:31 +0100

    numpad layout with grid

---
+++ NumPad/js/bundle.js
@@ -0,0 +1,7 @@
+(function(window, document){
+
+  const link = 
+    document.getElementsByClassName("item__link")[0];
+  const slidingWindow = 
+    document.getElementsByClassName("slidingItem")[0];
+})(window, document);
